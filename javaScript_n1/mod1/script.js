4f21d08e75a1f188a617468261998d4cc6b9f743
Christian Horst Haberler <christian@haberler.com.br>
2020-04-21 18:06:00 -0300

    inicio js

---
+++ javaScript_n1/mod1/script.js
@@ -0,0 +1,15 @@
+document.getElementById("titulo").innerHTML = "Opa, Tudo bem?";
+
+document.write("Algum Texto....");
+
+window.alert("Mensagem de exemplo.");
+
+var idade = 90;
+var nome = "TileWilly";
+window.alert(nome + " tem " + idade + " anos.");
+
+
+
+console.log("Esta mensagem vai aparecer no console");
+
+
